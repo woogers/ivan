8e32af959af77c46a79f9e3e5c0cceed65a87189
woogers <woogers@gmail.com>
2022-09-17 11:22:32 -0400

    initial commit

---
+++ markers.js
@@ -0,0 +1,44 @@
+var markers={
+  "normal": [
+    {
+      "checked": true,
+      "createInfoWindow": true,
+      "displayName": "Towns",
+      "groupName": "Townse9b7_d8e6",
+      "icon": "icons/town_hall.png",
+      "showIconInLegend": false
+    },
+    {
+      "checked": true,
+      "createInfoWindow": true,
+      "displayName": "Industry",
+      "groupName": "Industry1641_d8e6",
+      "icon": "signpost_icon.png",
+      "showIconInLegend": false
+    },
+    {
+      "checked": true,
+      "createInfoWindow": true,
+      "displayName": "Points of Interest",
+      "groupName": "Points_of_Intereste9a6_d8e6",
+      "icon": "signpost_icon.png",
+      "showIconInLegend": false
+    },
+    {
+      "checked": true,
+      "createInfoWindow": true,
+      "displayName": "Players",
+      "groupName": "Players1630_d8e6",
+      "icon": "signpost_icon.png",
+      "showIconInLegend": false
+    },
+    {
+      "checked": true,
+      "createInfoWindow": true,
+      "displayName": "CityRail Stations",
+      "groupName": "CityRail_Stations1652_d8e6",
+      "icon": "icons/rail_metro.png",
+      "showIconInLegend": false
+    }
+  ]
+};
---
+++ markersDB.js
@@ -0,0 +1,139 @@
+var markersDB={
+  "CityRail_Stations1652_d8e6": {
+    "created": false,
+    "name": "CityRail Stations",
+    "raw": [
+      {
+        "hovertext": "Central\nLines:1 ",
+        "text": "<img src=\"https://i.imgur.com/8Dsurt2.png\" width=\"300\"><br \\>Central Station, located in the Center City Tunnel, is the main station on the CityRail network. As implied by the name, it is centrally located, near the First Shelter, Factory, and Crafting Center.",
+        "x": 690,
+        "y": 64,
+        "z": 10
+      },
+      {
+        "hovertext": "Old Town\nLines:1 ",
+        "text": "<img src=\"https://i.imgur.com/FUo35Cw.png\" width=\"300\"><br \\>Old Town Station, located in the Center City Tunnel, serves the southern end of the Center City and Old Town village.",
+        "x": 690,
+        "y": 64,
+        "z": 134
+      },
+      {
+        "fill": false,
+        "hovertext": "Line 1",
+        "isLine": true,
+        "points": [
+          {
+            "x": 690,
+            "y": 64,
+            "z": 10
+          },
+          {
+            "x": 690,
+            "y": 64,
+            "z": 134
+          }
+        ],
+        "strokeColor": "#FFCB0C",
+        "strokeWeight": 2,
+        "text": "Line 1",
+        "x": 9223372036854775807,
+        "y": 9223372036854775807,
+        "z": 9223372036854775807
+      }
+    ]
+  },
+  "Industry1641_d8e6": {
+    "created": false,
+    "name": "Industry",
+    "raw": [
+      {
+        "hovertext": "Crafting Center",
+        "icon": "icons/factory.png",
+        "text": "Crafting Center",
+        "x": 702,
+        "y": 64,
+        "z": -4
+      },
+      {
+        "hovertext": "Create Factory",
+        "icon": "icons/factory.png",
+        "text": "Create Factory",
+        "x": 676,
+        "y": 64,
+        "z": -4
+      },
+      {
+        "hovertext": "CityRail Union Railyard and Shops",
+        "icon": "icons/factory.png",
+        "text": "CityRail Union Railyard and Shops",
+        "x": 830,
+        "y": 64,
+        "z": -22
+      }
+    ]
+  },
+  "Players1630_d8e6": {
+    "created": false,
+    "name": "Players",
+    "raw": [
+      {
+        "hovertext": "Last known location for narokuu",
+        "icon": "https://overviewer.org/avatar/narokuu",
+        "text": "Last known location for narokuu",
+        "x": 533.089410077609,
+        "y": 87.0,
+        "z": 91.70224576538409
+      },
+      {
+        "hovertext": "Last known location for wootnik",
+        "icon": "https://overviewer.org/avatar/wootnik",
+        "text": "Last known location for wootnik",
+        "x": 862.9795310505425,
+        "y": 70.125,
+        "z": -17.273971225958476
+      },
+      {
+        "hovertext": "Last known location for CrippyCrip",
+        "icon": "https://overviewer.org/avatar/CrippyCrip",
+        "text": "Last known location for CrippyCrip",
+        "x": 679.2375000119209,
+        "y": 72.10909999847412,
+        "z": 34.30000001192093
+      },
+      {
+        "hovertext": "Last known location for 10Toed",
+        "icon": "https://overviewer.org/avatar/10Toed",
+        "text": "Last known location for 10Toed",
+        "x": 678.3951539591512,
+        "y": 72.5625,
+        "z": 33.48449013835263
+      },
+      {
+        "hovertext": "Last known location for scottysnyder",
+        "icon": "https://overviewer.org/avatar/scottysnyder",
+        "text": "Last known location for scottysnyder",
+        "x": -3.3993644827588114,
+        "y": 71.0,
+        "z": 12.699999988079071
+      }
+    ]
+  },
+  "Points_of_Intereste9a6_d8e6": {
+    "created": false,
+    "name": "Points of Interest",
+    "raw": []
+  },
+  "Townse9b7_d8e6": {
+    "created": false,
+    "name": "Towns",
+    "raw": [
+      {
+        "hovertext": "City",
+        "text": "City",
+        "x": 690,
+        "y": 64,
+        "z": 28
+      }
+    ]
+  }
+};
